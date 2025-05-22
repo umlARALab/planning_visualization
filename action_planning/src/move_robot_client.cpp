4077febe24bd11f35c733b406e279c2015dd0c40
varadn <varadnev@gmail.com>
2025-05-22 12:19:04 -0400

    arm movement for picking in server

--- action_planning/src/move_robot_client.cpp
+++ action_planning/src/move_robot_client.cpp
@@ -1,23 +1,29 @@
 #include <functional>
 #include <memory>
 #include <thread>
+#include <vector>
+#include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include <geometry_msgs/msg/pose.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include "moveit_action/action/robot_move.hpp"
 
 class ArmActionClient : public rclcpp::Node {
  public:
     using RobotAction = moveit_action::action::RobotMove;
-    using GoalHandleTest = rclcpp_action::ServerGoalHandle<RobotAction>;
+    using GoalHandleTest = rclcpp_action::ClientGoalHandle<RobotAction>;
 
-    explicit ArmActionClient(const rclcpp::NodeOptions &options = rclcpp::NodeOptions()) : Node("arm_action_client", options) {
+    explicit ArmActionClient(const rclcpp::NodeOptions &options = rclcpp::NodeOptions()) : Node("action_client", options) {
         this->client_ptr_ = rclcpp_action::create_client<RobotAction>(this, "arm_action");
+
+        // status = rclcpp_action::GoalStatus::STATUS_EXECUTING;
+
     }
 
-    void send_goal(geometry_msgs::msg::Pose pose) {
+    void send_goal(std::vector<geometry_msgs::msg::Pose> pose) {
         // wait for server
         if (!this->client_ptr_->wait_for_action_server()) {
             RCLCPP_ERROR(this->get_logger(), "action server not available after waiting");
@@ -26,30 +32,104 @@ class ArmActionClient : public rclcpp::Node {
 
         // create goal
         auto goal_msg = RobotAction::Goal();
-        goal_msg.pose_goal = pose;
 
-        // send goal
-        RCLCPP_INFO(this->get_logger(), "Sending goal...");
+        // send goals to server one at a time; time out if it takes longer than a minute
+        int i = 1;
+        for (geometry_msgs::msg::Pose p : pose) {
+            RCLCPP_INFO(this->get_logger(), "Sending goal %d...", i);
+
+            // set goal
+            goal_msg.pose_goal = p;
+
+            // send goal to server when it is ready
+            if (!this->client_ptr_->wait_for_action_server(std::chrono::seconds(60))) {
+                RCLCPP_INFO(this->get_logger(), "[client] Action server not ready in time");
+            }
+
+            RCLCPP_INFO(this->get_logger(), "[client] Action server ready");
+
+            this->client_ptr_->async_send_goal(goal_msg);
+            rclcpp::sleep_for(std::chrono::seconds(10));
+
+            i++;
+        }
+
+        // auto goal_options = rclcpp_action::Client<RobotAction>::SendGoalOptions();
+        // goal_options.goal_response_callback = std::bind(%ArmActionClient::goal)
+        // this->client_ptr_->async_send_goal(goal_msg);
 
     }
 
  private:
     rclcpp_action::Client<RobotAction>::SharedPtr client_ptr_;
+    // rclcpp_action::GoalStatus status;
+
+    void goal_response_callback(std::shared_future<GoalHandleTest::SharedPtr> future) {
+        auto goal_handle = future.get();
+        if (!goal_handle) {
+            RCLCPP_ERROR(this->get_logger(), "Goal rejected by server.");
+        } else {
+            RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result.");
+        }
+    }
+
+    // void feedback_callback(GoalHandleTest::SharedPtr ptr, const std::shared_ptr<const RobotAction::Feedback> feedback) {
+    //     RCLCPP_INFO(this->get_logger(), feedback->progress);
+    // }
+
+    void result_callback(const GoalHandleTest::WrappedResult &result) {
+        switch (result.code)
+        {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal aborted");
+            break;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal canceleld");
+            break;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown goal result");
+            break;
+        }
+    }
 
 }; // Arm Action Client Class
 
 int main (int argc, char* argv[]) {
     rclcpp::init(argc, argv);
 
+    // std::chrono::duration<int> sleep_time = (std::chrono::seconds) 30;
     auto client = std::make_shared<ArmActionClient>();
 
+    // create vector of points
+    std::vector<geometry_msgs::msg::Pose> pose_list;
+
+    // use test pose to see if it works;
     geometry_msgs::msg::Pose test_pose;
     test_pose.position.x = 0.5;
     test_pose.position.y = 0.5;
-    test_pose.position.z = 0.5;
-    test_pose.orientation.w = 1.0;
+    test_pose.position.z = 0.25;
 
-    client->send_goal(test_pose);
+    tf2::Quaternion rot, start, end;
+    start.setRPY(0.0, 0.0, 0.0);
+    rot.setRPY(M_PI, 0.0, 0.0);
+    end = rot * start;
+    end.normalize();
+    test_pose.orientation = tf2::toMsg(end);
+
+    geometry_msgs::msg::Pose test2;
+    test2.position.x = 0.3;
+    test2.position.y = 0.5;
+    test2.position.z = 0.5;
+    test2.orientation = tf2::toMsg(end);
+
+    pose_list.push_back(test_pose);
+    pose_list.push_back(test2);
+
+    client->send_goal(pose_list);
+
+    // rclcpp::sleep_for(sleep_time);
 
     rclcpp::spin(client);
     rclcpp::shutdown();
--- action_planning/src/move_robot_server.cpp
+++ action_planning/src/move_robot_server.cpp
@@ -5,11 +5,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include <geometry_msgs/msg/pose.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
 // #include <geometry_msgs/msg/twist.hpp>
 
 #include "moveit_action/action/robot_move.hpp"
 #include <moveit/move_group_interface/move_group_interface.h>
 
+// action class
 class ArmActionServer : public rclcpp::Node {
  public:
     using RobotAction = moveit_action::action::RobotMove;
@@ -17,7 +20,8 @@ class ArmActionServer : public rclcpp::Node {
     using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
 
     // initialize action server for the arm
-    explicit ArmActionServer(const rclcpp::NodeOptions &options =  rclcpp::NodeOptions()) : Node("robot_action_server", options) {
+    explicit ArmActionServer(const rclcpp::NodeOptions &options =  rclcpp::NodeOptions()) : Node("action_server", options) {
+        // initialize action server with the name arm_action
         this->action_server_ = rclcpp_action::create_server<RobotAction>(
             this, 
             "arm_action",
@@ -26,27 +30,30 @@ class ArmActionServer : public rclcpp::Node {
             std::bind(&ArmActionServer::accept_goal, this, std::placeholders::_1)
         );
 
-        move_group_node_ = rclcpp::Node::make_shared("robot_action_server", options);
+        // initialize move group node
+        move_group_node_ = rclcpp::Node::make_shared("manipulator", options);
         executor_.add_node(move_group_node_);
         std::thread([this]() {this->executor_.spin(); }).detach();
 
+        // initialize interface; panda arm is the moveit demo robot 
         move_group_interface_ = std::make_shared<MoveGroupInterface>(move_group_node_, "panda_arm");
 
-        auto const logger = rclcpp::get_logger("arm_action");
+        // initialize logger for output
+        auto const logger = rclcpp::get_logger("action_server");
+        RCLCPP_INFO_ONCE(this->get_logger(), "\n[robot server: starting]\n");
     }
 
  private:
     rclcpp_action::Server<RobotAction>::SharedPtr action_server_;
-    // rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     rclcpp::Node::SharedPtr move_group_node_;
     rclcpp::executors::SingleThreadedExecutor executor_;
     std::shared_ptr<MoveGroupInterface> move_group_interface_;
 
-
+    // accept or reject the goal 
     rclcpp_action::GoalResponse goal_callback(const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const RobotAction::Goal> goal) {
-        // if the goal is in a specific range (?), execute
-        if (goal->pose_goal.position.x <= 1 || goal->pose_goal.position.x >= -1) {
-            // this->get_logger().info("\n[robot server: goal accepted]\n");
+        // if the goal is within a threshold (?), execute
+        // need to experiment with threshold values (1)
+        if (goal->pose_goal.position.x <= 1 && goal->pose_goal.position.x >= -1) {
             RCLCPP_INFO_ONCE(this->get_logger(), "\n[robot server: goal accepted]\n");
             (void)uuid;
             
@@ -58,47 +65,87 @@ class ArmActionServer : public rclcpp::Node {
         }
     }
 
+    // cancel the request
     rclcpp_action::CancelResponse cancel_callback(const std::shared_ptr<GoalHandleTest> goal_handle) {
             RCLCPP_INFO_ONCE(this->get_logger(), "\n[robot server: action cancelled]\n");
             (void)goal_handle;
             return rclcpp_action::CancelResponse::ACCEPT;
     }
 
+    // do stuff when the goal is executed
     void accept_goal(std::shared_ptr<GoalHandleTest> goal_handle) {
         // // needs to return quickly to avoid blocking executor --> spin new thread
         // std::thread{std::bind(&ArmActionServer::execute, this, std::placeholders::_1), goal_handle}.detach();
+        RCLCPP_INFO_ONCE(this->get_logger(), "\n[robot server: planning]\n");
+
+        // initialize container for list of waypoints
+        std::vector<geometry_msgs::msg::Pose> waypoints;
+        double eef_step = 0.01;
+        double jump = 0.0;
+        moveit_msgs::msg::RobotTrajectory trajectory_plan;
+
+        tf2::Quaternion rot, start, end;
+        start.setRPY(0.0, 0.0, 0.0);
+        rot.setRPY(M_PI, 0.0, 0.0);
+        end = rot * start;
+        end.normalize();
 
         // GET REQUEST FROM GOALS
         const auto goal_pose = goal_handle->get_goal();
         auto result = std::make_shared<RobotAction::Result>();
 
+        // ADD WAYPOINTS
+        // prepare to pick
+        geometry_msgs::msg::Pose start_point = goal_pose->pose_goal;
+        start_point.position.z += 0.1;
+        start_point.orientation = tf2::toMsg(end);
+        waypoints.push_back(start_point);
+
+        // lower to pick
+        geometry_msgs::msg::Pose obj_point = start_point;
+        obj_point.position.z -= 0.1;
+        obj_point.orientation = tf2::toMsg(end);
+        waypoints.push_back(obj_point);
+
+        // pick up
+        geometry_msgs::msg::Pose pick_point = obj_point;
+        pick_point.position.z += 0.1;
+        pick_point.orientation = tf2::toMsg(end);
+        waypoints.push_back(pick_point);
+
+        // place at target
+        geometry_msgs::msg::Pose target_point;
+        target_point.position.x = 0.5;
+        target_point.position.y = -0.5;
+        target_point.position.z = 0.25;
+        target_point.orientation = tf2::toMsg(end);
+        waypoints.push_back(target_point);
+
         // EXECUTE ACTION
-        // initialize moveit interface group
         // set target pose
-        move_group_interface_->setPoseTarget(goal_pose->pose_goal);
+        // move_group_interface_->setPoseTarget(goal_pose->pose_goal);
+
+        double path_result = move_group_interface_->computeCartesianPath(waypoints, eef_step, jump, trajectory_plan);
 
         // create plan to goal
-        MoveGroupInterface::Plan plan;
-        bool success = (move_group_interface_->plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
-        // auto const [success, plan] = [move_group_interface_]{
-        //     moveit::planning_interface::MoveGroupInterface::Plan msg;
-        //     auto const ok = static_cast<bool>(move_group_interface_->plan(msg));
-        //     return std::make_pair(ok, msg);
-        // }();
-
+        // MoveGroupInterface::Plan plan;
+        // bool success = (move_group_interface_->plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
 
         // execute plan
-        if (success) {
+        if (path_result >= 0.0) {
             RCLCPP_INFO_ONCE(this->get_logger(), "\n[robot server: executing]\n");
 
-            move_group_interface_->execute(plan);
+            move_group_interface_->execute(trajectory_plan);
 
             // SEND RESULT
             result->result = "Execution success";
             goal_handle->succeed(result); 
+            
         } else {
             RCLCPP_ERROR(this->get_logger(), "Planning failed");
-        }     
+        }
+        // clear waypoints
+        waypoints.clear();
     }
 
     // void execute(const const std::shared_ptr<GoalHandleTest> goal_handle) {
